654f37c897b22efe1c26872d2e0a16b23e7bc8b1
liusong <liusong@glaway.com>
2019-11-12 21:28:42 +0800

    测试push

--- src/main/java/applet/MyFirstApplet.java
+++ src/main/java/applet/MyFirstApplet.java
@@ -4,7 +4,7 @@ import java.applet.Applet;
 import java.awt.Graphics;
 
 public class MyFirstApplet extends Applet {
-	
+	//测试git上传
 	public void paint(Graphics g){
 		g.drawString("hello world", 100, 100);
 	}
