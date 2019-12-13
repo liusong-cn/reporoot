0cc6e2cc58116bd5ba060e3950e300d248da1b28
liusong <liusong@glaway.com>
2019-12-13 16:57:38 +0800

    文件加锁以及大文件的读写

--- src/main/java/com/learning/test/charpter18/FilePartLock.java
+++ src/main/java/com/learning/test/charpter18/FilePartLock.java
@@ -1,7 +1,10 @@
 package com.learning.test.charpter18;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 
@@ -10,14 +13,27 @@ import java.nio.channels.FileLock;
  */
 public class FilePartLock {
 
+    private static final int LENGTH = 0x8FFFFFF;//128M
+
+    private static FileChannel fc;
+
+    public static void main(String[] args) throws IOException {
+        fc = new RandomAccessFile("d://test.dat","rw").getChannel();
+        MappedByteBuffer mb = fc.map(FileChannel.MapMode.READ_WRITE,0,LENGTH);
+        for (int i = 0; i < LENGTH; i++) {
+            mb.put((byte)'x');
+        }
+        //由于在带参构造函数中已经调用了start（），故此处不用显示调用
+        new LockAndModify(mb,0,4);
+        new LockAndModify(mb,6,9);
+    }
+
     private static class LockAndModify extends Thread{
         private int start;
 
         private int end;
 
         private ByteBuffer buff;
-
-        private FileChannel fc;
 
         public LockAndModify(ByteBuffer bbf, int start, int end){
             this.start = start;
@@ -33,7 +49,12 @@ public class FilePartLock {
         public void run() {
             try {
                 FileLock fl = fc.lock(start, end, false);
-                
+                System.out.println("start lock: start " + start + " end " + end );
+                while (buff.position() < buff.limit() - 1){
+                    buff.put((byte)(buff.get() + 1));
+                }
+                //手动释放锁
+                fl.release();
             } catch (IOException e) {
                 e.printStackTrace();
             }
