76c88fce656dcb0f0b7ce1e83c39ade20b559e9c
liusong <1141185877@qq.com>
2020-09-17 15:52:15 +0800

    修改适配器模式

--- src/main/java/com/common/adaptermode1/Adapter.java
+++ src/main/java/com/common/adaptermode1/Adapter.java
@@ -9,6 +9,7 @@ public class Adapter  extends Adaptee implements Target{
 	@Override
 	public void requestAdater() {
 		//使用了父类adaptee的方法，此处可以加上控制等功能
-		getAge();
+		int age = getAge();
+		System.out.println("此处没做限制，调用了适配者/被代理者的方法，使得target增加了能力");
 	}
 }
--- src/main/java/com/common/adaptermode1/AdapterModeDemo.java
+++ src/main/java/com/common/adaptermode1/AdapterModeDemo.java
@@ -3,6 +3,7 @@ package com.common.adaptermode1;
 public class AdapterModeDemo {
 	
 	public static void main(String[] args) {
-
+		Target t = new Adapter();
+		t.requestAdater();
 	}
 }
